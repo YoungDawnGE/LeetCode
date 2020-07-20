ee0bbfffa26c5e85292e9f100b479c788696c07f
YoungDawnGe <geyangchengyc@163.com>
2020-07-20 17:14:24 +0800

    TwoSum2

---
+++ src/A161_170/A167_TwoSum2/TwoSum2.java
@@ -0,0 +1,51 @@
+package A161_170.A167_TwoSum2;
+
+/**
+ * Created by GYC
+ * 2020/7/20 16:59
+ * 167. 两数之和 II - 输入有序数组
+ * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+ *
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+ *
+ * 说明:
+ *
+ * 返回的下标值（index1 和 index2）不是从零开始的。
+ * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ * 示例:
+ *
+ * 输入: numbers = [2, 7, 11, 15], target = 9
+ * 输出: [1,2]
+ * 解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ */
+public class TwoSum2 {
+    public static void main(String[] args) {
+        int[] input = {2, 7, 11, 15};
+        int target = 15;
+
+        int[] res = new TwoSum2().twoSum(input, target);
+        for (int a : res) {
+            System.out.print(a+" ");
+        }
+    }
+
+    //way1 双指针 未用hashmap 第一道题两数之和用的是hashmap
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers.length <= 1) {
+            return null;
+        }
+        int l = 0;
+        int r = numbers.length-1;
+
+        while(l != r){
+            if (numbers[l] + numbers[r] > target) {
+                r--;
+            } else if (numbers[l] + numbers[r] < target) {
+                l++;
+            } else {
+                return new int[]{l + 1, r + 1};
+            }
+        }
+        return null;
+    }
+}
