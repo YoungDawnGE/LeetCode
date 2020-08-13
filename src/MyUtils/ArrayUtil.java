41764a179b420754290c47530c7a8aa6e24577b9
YoungDawnGe <geyangchengyc@163.com>
2020-08-13 15:16:47 +0800

    ArrayUtil

--- src/MyUtils/ArrayUtil.java
+++ src/MyUtils/ArrayUtil.java
@@ -4,8 +4,36 @@ package MyUtils;
  * Created by GYC
  * 2020/8/13 15:08
  */
-public class ArrayUtil<T> {
-    public static void printArray(T[][] array) {
+public class ArrayUtil {
+    public static void printArray(boolean[][] array){
+        int height = array.length;
+        if (height == 0) {
+            System.out.println("null");
+            return;
+        }
+        int width = array[0].length;
+        for (int i = 0; i <height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(" " + array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public static void printArray(int[][] array){
+        int height = array.length;
+        if (height == 0) {
+            System.out.println("null");
+            return;
+        }
+        int width = array[0].length;
+        for (int i = 0; i <height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(" " + array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public static void printArray(char[][] array){
         int height = array.length;
         if (height == 0) {
             System.out.println("null");
