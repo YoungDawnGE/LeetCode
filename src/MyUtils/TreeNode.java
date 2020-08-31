f3b1efde987dacb0440a5cca1e52d554d7789dc1
YoungDawnGe <geyangchengyc@163.com>
2020-08-31 10:19:18 +0800

    TreeNode

---
+++ src/MyUtils/TreeNode.java
@@ -0,0 +1,13 @@
+package MyUtils;
+
+/**
+ * Created by GYC
+ * 2020/8/31 10:11
+ * 树节点
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
